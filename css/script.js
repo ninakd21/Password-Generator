79d984d8f9bb67e9687ce77c886c0b32aa7e2753
Nina Davis <nina.k.davis2@gmail.com>
2021-08-16 16:50:35 -0500

    paste from local branch

--- css/script.js
+++ css/script.js
@@ -1,11 +1,51 @@
 // Assignment code here
 
-/* passwrod information variables*/
-var charecterlength = []
-var lowercase = []
-var upperCase = []
-var numbers = []
-var specialCharecters = []
+/* password information variables*/
+var charecterlength = '1234567890';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '0123456789';
+var specialCharecters = '!@#$%^&*()';
+var passwordData = '';
+
+function generatePassword() {
+
+  var charecterLength = window.prompt("Please select a number between 8 and 128 to be the length of your password.")
+
+  if (parseInt(charecterLength) <= 128 && parseInt(charecterLength) >= 8) {
+    var use_lowerCase = window.confirm('Would you like to include lowercase letters?')
+    var use_upperCase = window.confirm('Would you like to include uppercase letters?')
+    var use_numbers = window.confirm('Would you like to include numbers?')
+    var use_specialCharecters = window.confirm('Would you like to include special charecters?')
+    var password = " ";
+
+
+    if (use_lowerCase == 1) {
+      passwordData = passwordData + lowercase;
+  };
+  if (use_upperCase == 1) {
+    passwordData = passwordData + uppercase;
+  };
+  if (use_numbers == 1) {
+    passwordData = passwordData + numbers;
+  };
+  if (use_specialCharecters == 1) {
+    passwordData = passwordData + specialCharecters;
+  };
+  if (passwordData < 1) {
+    alert("You must choose to include at least one of the password criteria to genorate a password. Please start over.")
+  };
+  for (var i = 0; i < charecterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * passwordData.length);
+    password = password + passwordData[randomIndex];
+  }
+  return password;
+} else {
+  generatePassword();
+}
+};
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -17,51 +57,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-// Special characters for the function created
-const specialCharacters = "!@#$%^&*()";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-
-  var charecterLength = prompt("Please select a number between 8 and 128 to be the length of your password.");
-
-if (charecterLength < 8) { 
-  window.alert("Number is invalid! please pick a number between 8 and 128");
-  return charecterLength();
-}
-
-if (charecterLength > 128) {
-  window.alert("Number is invalid! please pick a number between 8 and 128");
-  return;
-}
-  // ask if they would like lowercase letters
-  var lowerCase = window.prompt('Would you like to include lowercase letters? Enter "YES" or "NO" to choose.');
-  if (lowerCase === "yes") { }
-    // record the answer yes
-    else if (lowerCase === "no") {}
-      // continue to next question
-  
-  //ask if they would like uppercse letters
-  var upperCase = window.prompt('Would you like to include uppercase letters? Enter "YES" or "NO" to choose.');
-  if (upperCase === "yes") { }
-    // record the answer yes
-    else if (uppercase === "no") {}
-      // continue to next question
-  
-  // ask if they want numbers
-  var numbers = window.prompt('Would you like to include numbers? Enter "YES" or "NO" to choose.');
-  if (numbers === "yes") { }
-    // record the answer yes
-    else if (numbers === "no") {}
-      // continue to next question
-  
-  // ask if they want special charecters
-  var specialCharecters = window.prompt('Would you like to include special charecters? Enter "YES" or "NO" to choose.');
-  if (specialCharecters === "yes") { }
-    // record the answer yes
-    else if (specialCharecters === "no") {}
-      // continue to next question
-  };
-generatePassword();
